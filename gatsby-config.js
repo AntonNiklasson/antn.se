467a16928417cab9b18610c25d131b149b6f0ade
Anton Niklasson <niklasson.anton@gmail.com>
2018-05-23 18:19:47 +0200

    add GA tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: 'UA-13287140-7'
       }
     },
     `gatsby-plugin-feed`,
