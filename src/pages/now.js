25971a0db1f51b5c5c68c81ee1b528eca68e22d6
Anton Niklasson <niklasson.anton@gmail.com>
2018-07-30 22:26:54 +0200

    add a now page template

---
+++ src/pages/now.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "gatsby-link";
+
+class NowPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>/now</h2>
+      </div>
+    );
+  }
+}
+
+export default NowPage;
