0a139f632e2e00bdce8f322607b2c1c141bd5f3a
Anton Niklasson <antonniklasson@Antons-MacBook-Pro.local>
2018-10-16 13:06:31 +0200

    updated /now

--- src/pages/now.js
+++ src/pages/now.js
@@ -8,18 +8,30 @@ const Content = styled.div``;
 
 const content = `
 
-📅 Lastupdated: 2018-09-28
 
-**Personal:** The focus right now includes nailing a few healthy habits. Specifically I would like to meditate every morning and read every night. Also working out consistently about three times per week is a big one.
+🧠🤔 Here's what's on my mind right now: 
 
-**Work:** I am currently available as a freelancer. I am pretty well seasoned in React, but I
-am definitely open to learn something like Vue or Angular. I have also worked a lot with Node, MongoDB, some ElasticSearch.
-  The most important thing would probably be the team. I want to find new challenges and new people to work with. I confident I can learn whatever
-is neccessary for the assignment. I love a challenge.
-
-  I was previously employed at [Netlight](https://www.netlight.com/) as a consultant. I sent a bit over at Bonnier News, building parts of their new publishing platform.
+- _I want to get back to working out and meditating regularly_ 🧘‍♂️
+- Solo-travel... 🌎
+- I want to learn Scala or something similar 👨‍💻
 
 ---
+
+I recently founded my own company - I am now available as a freelancer. I have done a lot of React, Redux, React Native and Node.js previously.
+
+I spend some time working on a few different projects, some or publicly available some are not. Here's a few:
+
+- [LearnReact.xyz](http://www.learnreact.xyz)
+- [CSSGrid.pro](http://www.cssgrid.pro)
+- [react-dategrid](https://www.github.com/AntonNiklasson/react-dategrid)
+- [react-clickcopy](https://www.github.com/AntonNiklasson/react-clickcopy)
+- [cv.antonniklasson.se](https://cv.antonniklasson.se)
+
+I was previously employed at [Netlight](https://www.netlight.com/) as a consultant. I spent about a year and a half at Bonnier News building parts of their new publishing platform.
+
+---
+
+*📅 Last updated: 2018-10-16*
 
 This is a [now page](https://sivers.org/nowff).
 `;
