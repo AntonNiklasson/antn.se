e5cf798ba018cb6d7e7a62ea470259f5c0142be2
Anton Niklasson <niklasson.anton@gmail.com>
2018-10-21 21:40:45 +0200

    update /now

--- src/pages/now.js
+++ src/pages/now.js
@@ -1,49 +1,56 @@
-import React from "react";
-import Link from "gatsby-link";
-import styled from "styled-components";
-import Markdown from "react-markdown";
-import Stats from "../components/stats";
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+import Markdown from 'react-markdown'
+import Stats from '../components/stats'
 
-const Content = styled.div``;
+const Content = styled.div`
+  footer {
+    display: flex;
+    justify-content: space-between;
+    margin: 3em 0 0 0;
+    padding: 1em 0;
+    border-top: 1px solid #ccc;
+    font-weight: bold;
+    font-size: 14px;
+  }
+`
 
 const content = `
-
-
 🧠🤔 Here's what's on my mind right now: 
 
-- _I want to get back to working out and meditating regularly_ 🧘‍♂️
-- Solo-travel... 🌎
-- I want to learn Scala or something similar 👨‍💻
+- 🧘‍♂️ _I want to get back to working out and meditating regularly_
+- 🌎 Solo travel
+- 👨‍💻 I want to learn Scala or something similar
 
 ---
 
-I recently founded my own company - I am now available as a freelancer. I have done a lot of React, Redux, React Native and Node.js previously.
+I recently founded my own company - I am now available as a freelancer! 😍 I have done a lot of React, Redux, React Native and Node.js previously.
 
-I spend some time working on a few different projects, some or publicly available some are not. Here's a few:
+I spend some time working on different projects, some are publicly available some are not. Here's a few:
 
 - [LearnReact.xyz](http://www.learnreact.xyz)
 - [CSSGrid.pro](http://www.cssgrid.pro)
 - [react-dategrid](https://www.github.com/AntonNiklasson/react-dategrid)
 - [react-clickcopy](https://www.github.com/AntonNiklasson/react-clickcopy)
-- [cv.antonniklasson.se](https://cv.antonniklasson.se)
 
 I was previously employed at [Netlight](https://www.netlight.com/) as a consultant. I spent about a year and a half at Bonnier News building parts of their new publishing platform.
-
----
-
-*📅 Last updated: 2018-10-16*
-
-This is a [now page](https://sivers.org/nowff).
-`;
+`
 
 class NowPage extends React.Component {
   render() {
     return (
       <Content>
         <Markdown source={content} />
+        <footer>
+          <span>📅 Last updated: 2018-10-21</span>
+          <span>
+            This is a <a href="https://sivers.org/nowff">now page</a>
+          </span>
+        </footer>
       </Content>
-    );
+    )
   }
 }
 
-export default NowPage;
+export default NowPage
