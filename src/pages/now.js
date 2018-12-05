b4aff97005cb3d2ab24c364786cc53b4a17efeef
Anton Niklasson <niklasson.anton@gmail.com>
2018-12-05 13:09:43 +0100

    update /now

--- src/pages/now.js
+++ src/pages/now.js
@@ -20,22 +20,16 @@ const Content = styled.div`
 const content = `
 ${emoji.get('thinking_face')} Here's what's on my mind right now: 
 
-- ${emoji.get(
-  'running'
-)} _I want to get back to working out and meditating regularly_
-- 🌎 Solo travel
-- 👨‍💻 I want to learn Scala or something similar
+- ${emoji.get('running')} I want to get back to working out and meditating regularly
+- 🌎 Travels. Something might be revealed in my [NomadList profile](https://www.nomadlist.com/@antonniklasson) shortly ${emoji.get('smirk')}
+
+---
+
+I spend some time working on different projects, some are publicly available some are not. The one I'm the most into right now is *[Shortcuts.rocks](http://www.shortcuts.rocks)*. Some others are [LearnReact.xyz](http://www.learnreact.xyz), [CSSGrid.pro](http://www.cssgrid.pro), [react-dategrid](https://www.github.com/AntonNiklasson/react-dategrid) and [react-clickcopy](https://www.github.com/AntonNiklasson/react-clickcopy).
 
 ---
 
 I recently founded my own company - I am now available as a freelancer! 😍 I have done a lot of React, Redux, React Native and Node.js previously.
-
-I spend some time working on different projects, some are publicly available some are not. Here's a few:
-
-- [LearnReact.xyz](http://www.learnreact.xyz)
-- [CSSGrid.pro](http://www.cssgrid.pro)
-- [react-dategrid](https://www.github.com/AntonNiklasson/react-dategrid)
-- [react-clickcopy](https://www.github.com/AntonNiklasson/react-clickcopy)
 
 I was previously employed at [Netlight](https://www.netlight.com/) as a consultant. I spent about a year and a half at Bonnier News building parts of their new publishing platform.
 `
@@ -46,7 +40,7 @@ class NowPage extends React.Component {
       <Content>
         <Markdown source={content} />
         <footer>
-          <span>📅 Last updated: 2018-10-21</span>
+          <span>📅 Last updated: 2018-12-05</span>
           <span>
             This is a <a href="https://sivers.org/nowff">now page</a>
           </span>
