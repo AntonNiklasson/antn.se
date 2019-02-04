3642263625ca690d93bf2395c70e900fcddb227c
Anton Niklasson <niklasson.anton@gmail.com>
2019-02-04 15:35:24 +0100

    improve the deployment git commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 ssh root@95.85.43.196 <<'ENDSSH'
 cd /var/www/antonniklasson.se
-git reset --hard HEAD && git clean -f -d
-git pull origin master
+git fetch origin
+git reset --hard origin/master
 yarn
 yarn build
 ENDSSH
