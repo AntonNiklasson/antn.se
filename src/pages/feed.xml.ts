9ff2e31089f38af0e359a2dbf5f214187885742c
Anton Niklasson <niklasson.anton@gmail.com>
2024-02-11 17:02:37 +0100

    add rss feed to /feed.xml

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,25 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context: any) {
+	console.log({
+		context,
+	});
+
+	const blog = await getCollection("blog");
+	const items = blog.map((post) => {
+		return {
+			title: post.data.title,
+			description: post.data.summary,
+			pubDate: post.data.date,
+			link: post.slug,
+		};
+	});
+
+	return rss({
+		title: "Anton Niklasson",
+		description: "",
+		site: context.site,
+		items,
+	});
+}
