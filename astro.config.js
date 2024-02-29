537473d83a5a2c3c34263a977ce7965f526f8345
Anton Niklasson <niklasson.anton@gmail.com>
2024-02-29 22:02:57 +0100

    relace cf web analytics with ga (#39)

--- astro.config.js
+++ astro.config.js
@@ -3,6 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import satori from "satori";
 import { generateOGImages } from "./generate-og-image";
+import partytown from "@astrojs/partytown";
 
 function og() {
 	return {
@@ -17,5 +18,14 @@ function og() {
 
 export default defineConfig({
 	site: "https://antn.se",
-	integrations: [sitemap(), mdx(), og()],
+	integrations: [
+		sitemap(),
+		mdx(),
+		og(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
 });
