e3a51a47905b3b5a0c0a6a4ea6efff2de36dd693
Anton Niklasson <niklasson.anton@gmail.com>
2024-03-06 14:45:22 +0100

    clean up post cover generation files

--- scripts/generate-post-covers/index.js
+++ scripts/generate-post-covers/index.js
@@ -2,6 +2,62 @@ import satori from "satori";
 import { Resvg } from "@resvg/resvg-js";
 import parseFrontmatter from "gray-matter";
 
+export function generatePostCovers() {
+	return {
+		name: "generate-post-images",
+		hooks: {
+			"astro:build:done": async ({ dir, pages }) => {
+				await generateOGImages(dir, pages);
+			},
+		},
+	};
+}
+
+async function generateOGImages(dir, pages) {
+	const width = 1200;
+	const height = 630;
+
+	const interFont = fs.readFileSync(`public/Inter-Medium.ttf`);
+	const interBoldFont = fs.readFileSync(`public/Inter-Bold.ttf`);
+
+	for (const { pathname } of pages) {
+		const meta = getPostMeta(pathname);
+
+		if (meta === null) continue;
+
+		console.log(`Generating cover for post "${meta.title}"`);
+
+		const svg = await satori(html(meta), {
+			width,
+			height,
+			fonts: [
+				{
+					name: "Inter",
+					data: interFont,
+					style: "normal",
+					weight: 400,
+				},
+				{
+					name: "Inter",
+					data: interBoldFont,
+					style: "normal",
+					weight: 600,
+				},
+			],
+		});
+
+		const renderer = new Resvg(svg, {
+			fitTo: {
+				mode: "width",
+				value: width,
+			},
+		});
+		const image = renderer.render().asPng();
+
+		fs.writeFileSync(`${dir.pathname}${pathname}cover.png`, image);
+	}
+}
+
 function html({ title, date }) {
 	return {
 		type: "div",
@@ -71,50 +127,5 @@ function getPostMeta(pathname) {
 	} catch (error) {
 		// Non-post pages will return null here
 		return null;
-	}
-}
-
-export async function generateOGImages(dir, pages) {
-	const width = 1200;
-	const height = 630;
-
-	const interFont = fs.readFileSync(`public/Inter-Medium.ttf`);
-	const interBoldFont = fs.readFileSync(`public/Inter-Bold.ttf`);
-
-	for (const { pathname } of pages) {
-		const meta = getPostMeta(pathname);
-
-		if (meta === null) continue;
-
-		console.log(`Generating cover for post "${meta.title}"`);
-
-		const svg = await satori(html(meta), {
-			width,
-			height,
-			fonts: [
-				{
-					name: "Inter",
-					data: interFont,
-					style: "normal",
-					weight: 400,
-				},
-				{
-					name: "Inter",
-					data: interBoldFont,
-					style: "normal",
-					weight: 600,
-				},
-			],
-		});
-
-		const renderer = new Resvg(svg, {
-			fitTo: {
-				mode: "width",
-				value: width,
-			},
-		});
-		const image = renderer.render().asPng();
-
-		fs.writeFileSync(`${dir.pathname}${pathname}cover.png`, image);
 	}
 }
