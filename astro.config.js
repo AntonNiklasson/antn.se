8084f23067f062169b41a93b2688d9e018307518
Anton Niklasson <niklasson.anton@gmail.com>
2025-07-07 23:30:17 +0200

    upgrade astro

--- astro.config.js
+++ astro.config.js
@@ -9,7 +9,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://antn.se",
-	output: "hybrid",
+	output: "server",
 	adapter: vercel(),
 	integrations: [
 		sitemap(),
