d594d38dccf700e1e74127d1481a59a7453f043e
Anton Niklasson <niklasson.anton@gmail.com>
2026-02-26 21:40:08 +0100

    refine contact form styling

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,18 @@
 import type { FC } from "react";
 
 type Props = {
-	label: string;
-	submit?: boolean;
-	disabled?: boolean;
+  label: string;
+  submit?: boolean;
+  disabled?: boolean;
 };
 
 export const Button: FC<Props> = ({ label, submit, disabled }) => {
-	return (
-		<button
-			type={submit ? "submit" : undefined}
-			disabled={disabled}
-			className="rounded border-2 border-gray-300 bg-transparent px-6 py-2 text-sm font-bold text-gray-400 transition hover:border-fuchsia-700 hover:text-fuchsia-700 hover:shadow focus:border-fuchsia-700 focus:text-fuchsia-700 focus:outline-hidden">
-			{label}
-		</button>
-	);
+  return (
+    <button
+      type={submit ? "submit" : undefined}
+      disabled={disabled}
+      className="rounded border-2 border-gray-300 bg-transparent px-6 py-2 text-xs text-gray-400 uppercase transition hover:border-fuchsia-700 hover:text-fuchsia-700 hover:shadow focus:border-fuchsia-700 focus:text-fuchsia-700 focus:outline-hidden">
+      {label}
+    </button>
+  );
 };
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -2,41 +2,39 @@ import type { FC } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
-	id: string;
-	label: string;
-	placeholder?: string;
-	multiline?: boolean;
-	autofocus?: boolean;
+  id: string;
+  label: string;
+  placeholder?: string;
+  multiline?: boolean;
+  autofocus?: boolean;
 };
 
 export const Input: FC<Props> = ({ id, label, placeholder, multiline, autofocus }) => {
-	const Component = multiline ? "textarea" : "input";
+  const Component = multiline ? "textarea" : "input";
 
-	return (
-		<div className="group flex flex-col items-start space-y-2">
-			<label
-				htmlFor={id}
-				className="text-xs font-bold text-gray-500 transition group-focus-within:text-gray-900">
-				{label}:
-			</label>
-			<div
-				className={twMerge(
-					"flex w-full bg-gray-300 transition group-focus-within:bg-gray-900",
-					multiline && "mt-2 pl-[2px]",
-					!multiline && "pb-[2px]",
-				)}>
-				<Component
-					id={id}
-					name={id}
-					placeholder={placeholder}
-					autoFocus={autofocus}
-					className={twMerge(
-						"h-full w-full bg-white p-2 text-sm font-semibold transition focus:outline-hidden",
-						!multiline && "",
-						multiline && "h-32 resize-none",
-					)}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="group flex flex-col items-start space-y-2">
+      <label htmlFor={id} className="text-xs font-bold text-gray-500 transition group-focus-within:text-gray-900">
+        {label}:
+      </label>
+      <div
+        className={twMerge(
+          "flex w-full bg-gray-300 transition group-focus-within:bg-gray-900",
+          multiline && "mt-2 pl-[1px]",
+          !multiline && "pb-[1px]",
+        )}>
+        <Component
+          id={id}
+          name={id}
+          placeholder={placeholder}
+          autoFocus={autofocus}
+          className={twMerge(
+            "h-full w-full bg-white p-2 text-sm font-semibold transition focus:outline-hidden",
+            !multiline && "",
+            multiline && "h-32 resize-none",
+          )}
+        />
+      </div>
+    </div>
+  );
 };
